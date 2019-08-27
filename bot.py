1fddce2e1695002900bfa69f98262bf5690d2657
Jordan Lund <jlund@mozilla.com>
2019-08-26 18:05:42 -0700

    sends overall status command to slack and formats blocks

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import aiohttp
 import logging
 import json
 import os
+import re
 import sys
 import copy
 
@@ -31,7 +32,7 @@ def add_divider():
 
 def expand_slack_payload(**payload):
     data = payload["data"]
-    message = data["text"]
+    message = data.get("text", "")
     web_client = payload["web_client"]
 
     return data, message, web_client
@@ -42,6 +43,7 @@ async def get(url, logger=LOGGER):
             if response.status != 200:
                 logger.error("Could not complete request. Are you connected to the VPN?")
                 logger.error(f"Failed to GET {response.url}: {response.status}; body={(await response.text())[:1000]}")
+                sys.exit()
             response = await response.json()
 
     return response
@@ -51,19 +53,20 @@ async def get_releases(logger=LOGGER):
     return await get(url)
 
 
-def add_release_signoff_status(reply, release):
-    reply = add_a_block(reply, add_section(f"*{release['name']}*"))
-    reply = add_a_block(reply, add_section("Status:"))
+def add_release_signoff_status(reply, release, logger=LOGGER):
+    reply = add_a_block(reply, add_section(f"Status: *{release['name']}*"))
     for phase in release["phases"]:
-        state = "requires signoff"
-        if phase["submitted"]:
-            if phase["completed"]:
-                state = ":white_check_mark:"
-            else:
-                state = ":arrows_counterclockwise:"
+        # strip the product name out of the phase for presenting
+        phase_name = re.sub("(_firefox|_thunderbird|_fennec)", "", phase["name"])
+        state = ":passport_control:"
+        tc_url = ""
+        if phase["submitted"] and phase["completed"]:
+            state = ":white_check_mark:"
+            tc_url = f"https://taskcluster-ui.herokuapp.com/tasks/{phase['actionTaskId']}"
 
-        reply = add_a_block(reply, add_section(f"\tPhase: {phase['name']} - {state}"))
+        reply = add_a_block(reply, add_section(f"    {phase_name} - {state} {tc_url}"))
     reply = add_a_block(reply, add_divider())
+    return reply
 
 def add_overall_status(reply, releases, logger=LOGGER):
     # compose message status
@@ -89,7 +92,7 @@ def add_bot_help(reply):
 @slack.RTMClient.run_on(event="message")
 async def receive_message(**payload):
 
-    data, message, web_client = expand_slack_payload(payload)
+    data, message, web_client = expand_slack_payload(**payload)
 
     # template reply
     reply = {
@@ -99,24 +102,26 @@ async def receive_message(**payload):
         "blocks": [],
     }
 
-    releases = await get_releases()
-    if "status" == message.lower():
-        reply = add_overall_status(reply, releases)
-    if "status" in message.lower():
-        for release in releases:
-            if release["name"] in message:
-                reply = add_release_status(reply, release)
-                break
+    LOGGER.debug(f"message: {message}")
+    if  message.lower().startswith("shipit"):
+        releases = await get_releases()
+        # TODO should probably use regex or click to parse commands
+        if "shipit status" == message.lower():
+            # overall status
+            reply = add_overall_status(reply, releases)
+        elif "shipit status" in message.lower():
+            # a more detailed specific release status
+            for release in releases:
+                if release["name"] in message:
+                    reply = add_release_status(reply, release)
+                    break
+            else:
+                reply = add_a_block(reply, add_section("No matching release status could be found. Message `shipit help` for usage"))
+                reply = add_a_block(reply, add_divider())
         else:
-            reply = add_a_block(reply, add_section("No matching release status could be found. See help"))
-            reply = add_a_block(reply, add_divider())
-            reply = add_bot_help(reply)
+            reply = add_a_block(reply, add_section("Sorry, I don't understand. Try messaging `shipit help` for usage"))
 
-    if reply["channel"] != "ID?": # TODO find out the ID for releaseduty
-        LOGGER.error("Can only reply to the #releaseduty channel. Is this bot coming from another channel?")
-        sys.exit()
-
-    await web_client.chat_postMessage(reply)
+        return await web_client.chat_postMessage(**reply)
 
 
 if __name__ == "__main__":
