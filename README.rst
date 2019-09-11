957280cacca5cc52cc38cb11a4463b7ee4b0441a
Jordan Lund <jlund@mozilla.com>
2019-09-11 15:44:52 -0700

    adds README and missing tc.py for getting group status

---
+++ README.rst
@@ -0,0 +1,33 @@
+# A Slack bot for monitoring releases in flight
+
+## Deployed on
+
+* staging: mozilla-sandbox-SCIM workspace. #releaseduty channel
+* prod mozilla workspace. #releng-notifications channel
+
+## Usage
+
+Supported queries:
+
+    * `shipit status`
+        * Shows each active release within shipit.mozilla-releng.net. Checks only what phases have been signed off. Doesn't inspect the Taskcluster graph status within a phase
+    * `shipit status $release`
+        * Shows each phase signoff status and inspects the most recent phase's Taskcluster graph status. Highlighting how far along the graph is and which (if any) tasks are stuck and require attention.
+        * $release: can be a substring of the full release name. e.g. 'Devedition' would match 'Devedition-70.0b5-build1'
+
+Background tasks (non interactive):
+    * every 2 min the bot will check for active releases in Shipit and ping @releaseduty if a phase's Taskcluster graph has one or more stuck tasks.
+
+## Hacking
+
+slackbot-release uses poetry. It's currently not packaged.
+
+```
+git clone https://github.com/lundjordan/slackbot-release.git
+cd slackbot-release
+# create your python env. e.g. with pyenv: pyenv virtualenv slackbot-release && pyenv local slackbot-release
+poetry install
+cp secrets.json.template secrets.json  # fill in slack token (ask jlund) and a tc client for inspecting tc groups
+# connect to vpn so you can communicate with shipit
+python slackbot_release/bot.py
+```
---
+++ slackbot_release/tc.py
@@ -0,0 +1,50 @@
+import asyncio
+from collections import namedtuple
+import logging
+import os
+
+import taskcluster.aio
+
+# TODO rip this out as part of a standalone group inspector module. Replace graph-progress.sh and tc-filter.py
+
+### logging
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
+
+async def get_taskcluster_group_status(graph_id, config, logger=LOGGER):
+
+    # reimplements tc-filter.py show_filtered
+    tc_options = {
+        "rootUrl": config["taskcluster_root_url"],
+        "credentials": {
+            "clientId": config["taskcluster_client_id"],
+            "accessToken": config["taskcluster_access_token"],
+        },
+    }
+
+    filtered_tasks = []
+    loop = asyncio.get_running_loop()
+
+    async with taskcluster.aio.createSession(loop=loop) as tc_session:
+        queue = taskcluster.aio.Queue(options=tc_options, session=tc_session)
+        def pagination(y):
+            filtered_tasks.extend(y.get('tasks', []))
+
+        await queue.listTaskGroup(graph_id, paginationHandler=pagination)
+
+    group_status = {
+        "unscheduled": [],
+        "pending": [],
+        "running": [],
+        "completed": [],
+        "failed": [],
+        "exception": [],
+    }
+    Task = namedtuple("Task", ["taskid", "label", "worker_type"])
+    for t in filtered_tasks:
+        label = t['task'].get('tags', {}).get('label', t['task'].get('metadata').get('name', ''))
+        task_status = t['status']
+        group_status[task_status['state']].append(
+            Task(task_status['taskId'], label, task_status['workerType'])
+        )
+    return group_status
